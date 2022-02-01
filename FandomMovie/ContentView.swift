d861a441d2ffef4ac59fac143786cf0b871391e6
reganlaurell <reganlaurell@gmail.com>
2022-01-31 20:16:02 -0500

    Initial Commit

---
+++ FandomMovie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FandomMovie
+//
+//  Created by Regan Laurell on 1/31/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FandomMovie/FandomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FandomMovieApp.swift
+//  FandomMovie
+//
+//  Created by Regan Laurell on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct FandomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
